b3d0bf116562e3a03981858357a36dd7520e547e
Lukam90 <lucien.hamm@gmail.com>
2023-02-21 23:11:33 +0100

    MAJ Back > Password Hash

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,7 +7,15 @@ const loginUser = async (req, res) => {
 
 // Signup user
 const signupUser = async (req, res) => {
+    const { email, password } = req.body
 
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({ email, password })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 module.exports = { loginUser, signupUser }
