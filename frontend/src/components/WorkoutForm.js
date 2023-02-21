6215d852560cd2f64f4e763abcc122c8665ef1a7
Lukam90 <lucien.hamm@gmail.com>
2023-02-21 21:45:36 +0100

    MAJ Frontend > Context

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,10 @@
 import { useState } from "react"
 
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
 const WorkoutForm = () => {
+    const { dispatch } = useWorkoutsContext()
+
     const [title, setTitle] = useState("")
     const [load, setLoad] = useState("")
     const [reps, setReps] = useState("")
@@ -31,6 +35,8 @@ const WorkoutForm = () => {
             setReps("")
 
             setError(null)
+
+            dispatch({ type: "CREATE_WORKOUT", payload: json })
         }
     }
 
